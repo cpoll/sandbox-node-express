20cebd15de680508788cdbf0e8939f1e2122f02c
Cristian Poll <cristian@cristianpoll.com>
2023-11-14 11:11:37 -0500

    test statsd

--- server.js
+++ server.js
@@ -6,8 +6,56 @@ const args = process.argv.slice(2);
 const app = express()
 const port = args[0] || 3000;
 
+// #region StatsD timer
+/*
+
+Defaults to connecting to DD_AGENT_HOST or localhost:8125 if that value is unset.
+If you've set up a DataDog Agent locally this should work as-is.
+
+You can find your metrics in DD under Metrics -> Summary (https://app.datadoghq.com/metric/summary)
+
+*/
+const statsDClient = new StatsD({})
+
+
+function sendStatsdMetrics() {
+  statsDClient.gauge("my_random_number_gauge", Math.floor(Math.random() * 10));
+}
+
+setInterval(sendStatsdMetrics, 10000);
+// #endregion
+
+// #region http Stats
+// Getting efficient http tags for sending to statsd, because custom metrics get expensive if there are too many permutations
+// {route: /asd/:org/:team } is good, {route: /asd/org1/team1} is too many
+app.use(function (req, res, next) {
+  const startTime = new Date().getTime();
+
+  console.log(req.url);
+  // console.log(req.route); -> undefined
+
+  res.on("finish", () => {
+    const endTime = new Date().getTime();
+    // console.log(req.route.path); -> /resource/:org/:team
+    // statsDClient.histogram("http_request_time", endTime-startTime, {route: req.url});
+  });
+
+  next();
+});
+// #endregion
+
+
+// #region Routes
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    new Promise((resolve) => {
+        setTimeout(() => { console.log('the promise finished'); resolve(); }, 10000);
+    });
+    res.send('Hello World!')
+})
+
+app.get('/resource/:org/:team', (req, res) => {
+  const { org, team } = req.params;
+  res.send(`org: ${org}, team: ${team}`);
 })
 
 // #region TEST PATHS
@@ -35,26 +83,9 @@ app.get('/', (req, res) => {
       a++;
     }
   });
-// #endregion
+// #endregion TEST PATHS
 
-// #region StatsD timer
-/*
-
-Defaults to connecting to DD_AGENT_HOST or localhost:8125 if that value is unset.
-If you've set up a DataDog Agent locally this should work as-is.
-
-You can find your metrics in DD under Metrics -> Summary (https://app.datadoghq.com/metric/summary)
-
-*/
-const statsDClient = new StatsD({})
-
-
-function sendStatsdMetrics() {
-  statsDClient.gauge("my_random_number_gauge", Math.floor(Math.random() * 10));
-}
-
-setInterval(sendStatsdMetrics, 10000);
-// #endregion
+// #endregion Routes
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
